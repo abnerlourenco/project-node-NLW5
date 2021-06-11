0ea7fd5250c4087c9a62f0dfd7be122b856b67d4
abnerlourenco <abner2010_hm@hotmail.com>
2021-06-11 18:32:25 -0300

    Create repositories and test routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 
 import "./database";
+import { routes } from "../routes";
 
 const app = express();
+
+app.use(express.json());
 
 /**
  * GET = Buscas 
@@ -12,18 +15,6 @@ const app = express();
  * PATCH = Alterar uma informação específica
  */
 
-app.get("/", (request, response ) => {
-  return response.json({
-    message: "Olá NLW 5",
-    id: 1,
-    hora: "12:59:45",
-  });
-});
-
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário cadastrado com sucesso!"
-  })
-})
+app.use(routes);
 
 app.listen(3333, () =>console.log("Server is running on port 3333"));
