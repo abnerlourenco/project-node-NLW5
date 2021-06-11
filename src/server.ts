19b010974d888376d5d5510984764ca64afe79cf
abnerlourenco <abner2010_hm@hotmail.com>
2021-06-11 13:17:15 -0300

    first test json response route

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,11 @@ const app = express();
  */
 
 app.get("/", (request, response ) => {
-  return response.send("Olá NLW 5");
+  return response.json({
+    message: "Olá NLW 5",
+    id: 1,
+    hora: "12:59:45",
+  });
 });
 
 app.listen(3333, () =>console.log("Server is running on port 3333"));
