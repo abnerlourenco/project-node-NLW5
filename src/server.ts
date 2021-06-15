b34259e738265663008c23dbb1a178b38037f427
abnerlourenco <abner2010_hm@hotmail.com>
2021-06-15 19:18:02 -0300

    added websocket and http services from socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path"
 
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+//criando uam rota de teste
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+})
+
+// criando protocolo http
+const http = createServer(app); 
+//criando protocolo websocket
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+})
 
 app.use(express.json());
 
@@ -17,4 +39,4 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () =>console.log("Server is running on port 3333"));
+http.listen(3333, () =>console.log("Server is running on port 3333"));
