002fe89178fe566093cafba2488b2d3170248607
abnerlourenco <abner2010_hm@hotmail.com>
2021-06-15 11:39:20 -0300

    Created the entity to messages table

---
+++ src/entities/Message.ts
@@ -0,0 +1,41 @@
+import {  
+  Entity, 
+  Column, 
+  CreateDateColumn, 
+  PrimaryColumn, 
+  ManyToOne,
+  JoinColumn} from "typeorm"
+
+import { v4 as uuid } from "uuid"
+import { User } from "./Users";
+
+@Entity("messages")
+class Message {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  text: string;
+
+  @JoinColumn({ name:"user_id" })
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Message}
